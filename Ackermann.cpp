80669634808d3aac3f11d43a653c9ae904f72f2c
Sergio0-O <sergioostria166@gmail.com>
2026-02-21 20:39:54 -0800

    Update Ackermann.cpp

--- Ackermann.cpp
+++ Ackermann.cpp
@@ -4,6 +4,11 @@ using namespace std;
 // Ackermann function definition
 unsigned long long ackermann(unsigned int m, unsigned int n) {
  // TODO: Complete the code.
+	if (m == 0){return n+1;}
+
+	if(m > 0 && n == 0){return ackermann(m-1,1);}
+
+	return ackermann(m-1,ackermann(m,n-1));
 }
 
 int main() {
@@ -11,7 +16,7 @@ int main() {
     cout << "Enter m and n: ";
     cin >> m >> n;
 
-    cout << "Ackermann(" << m << ", " << n << ") = " 
+    cout << "Ackermann(" << m << ", " << n << ") = "
          << ackermann(m, n) << endl;
 
     return 0;
